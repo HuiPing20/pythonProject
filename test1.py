c626e47a48bcbdf280eb211b9b797e88552db604
Parktool-8688 <2841561883@qq.com>
2024-12-26 23:24:03 +0800

    第三次提交

--- test1.py
+++ test1.py
@@ -1,24 +1,15 @@
-def find_last_remaining(n, m):
+def josephus_math(n, m):
     if n == 0:
         return 0
-
-    # 创建一个列表来表示环中的元素
-    circle = list(range(1, n + 1))
-
-    index = 0  # 开始报数的位置
-
-    while len(circle) > 1:
-        # 计算当前要移除的元素的索引
-        index = (index + m - 1) % len(circle)
-        # 移除该元素
-        circle.pop(index)
-
-    # 返回最后剩下的元素
-    return circle[0]
-
+    # 初始化最后一个人的位置为0（因为我们的索引是从0开始的）
+    pos = 0
+    for i in range(2, n + 1):
+        pos = (pos + m) % i
+    # 因为编号是从1开始的，所以需要将结果加1
+    return pos + 1
 
 # 示例调用
 n = 7  # 环中的人数
 m = 3  # 报数的步长
-last = find_last_remaining(n, m)
+last = josephus_math(n, m)
 print(f"最后剩下的人的编号是: {last}")
